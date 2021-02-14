8a14af0d95d1a350ba78824a2e71990671557706
Tech-Nayuta <ketanayutawork@gmail.com>
2021-02-14 22:15:57 +0900

    make simple index view

--- Colombia/Sources/Home/WorksIndexTabViewController.swift
+++ Colombia/Sources/Home/WorksIndexTabViewController.swift
@@ -12,6 +12,22 @@ class WorksIndexTabViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let worksIndexVC = WorksIndexViewController()
+        worksIndexVC.tabBarItem.title = "一覧"
+        worksIndexVC.tabBarItem.tag = 1
+        worksIndexVC.tabBarItem.image = UIImage(named: "document.png")
+        
+        let favoriteWorksIndexVC = WorksIndexViewController()
+        favoriteWorksIndexVC.tabBarItem.title = "お気に入り"
+        favoriteWorksIndexVC.tabBarItem.tag = 2
+        favoriteWorksIndexVC.tabBarItem.image = UIImage(named: "favorite.png")
+        
+        let vcList: [UIViewController] = [ worksIndexVC, favoriteWorksIndexVC ]
+        setViewControllers(vcList, animated: true)
+        
+//        worksIndexVC.tabBarItem.badgeValue = "5"
+//        worksIndexVC.tabBarItem.image = UIImage(named: "file")?.resize(width: 30)
+        
         // Do any additional setup after loading the view.
     }
 
