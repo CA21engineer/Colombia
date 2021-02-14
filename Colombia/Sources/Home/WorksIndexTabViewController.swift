7fa56bbd84cd7af79f5bbb4de2342a652e165cf9
Tech-Nayuta <ketanayutawork@gmail.com>
2021-02-14 16:40:13 +0900

    UI作成に必要なファイルの追加

---
+++ Colombia/Sources/Home/WorksIndexTabViewController.swift
@@ -0,0 +1,29 @@
+//
+//  IndexTabViewController.swift
+//  Colombia
+//
+//  Created by 化田晃平 on R 3/02/14.
+//
+
+import UIKit
+
+class WorksIndexTabViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
