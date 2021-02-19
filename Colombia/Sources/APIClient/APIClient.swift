9e3ec17c646e4ac69c3f98901cf20650d6880289
yamanetaisei <swiftmantaisei@gmail.com>
2021-02-19 17:39:36 +0900

    APIClientの処理を追加

--- Colombia/Sources/APIClient/APIClient.swift
+++ Colombia/Sources/APIClient/APIClient.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import RxSwift
+
+struct APIClient {
+    private let decoder: JSONDecoder = {
+        let decoder = JSONDecoder()
+        return decoder
+    }()
+    
+    func request<T: Requestable>(_ requestable: T) -> Observable<T.Response> {
+        Observable<T.Response>.create { observer in
+            let url = requestable.url
+            let request = URLRequest(url: url)
+            let task = URLSession.shared.dataTask(with: request) { resultData, response, error in
+                guard let resultData = resultData else { return }
+                do {
+                    let decodeData = try decoder.decode(T.Response.self, from: resultData)
+                    observer.onNext(decodeData)
+                } catch let error {
+                    print(error)
+                }
+            }
+            task.resume()
+            return Disposables.create()
+        }
+    }
+}
